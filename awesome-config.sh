6171652607b1b0bad190fea2ba293783a1d66e57
oguzhanlarca <oguzhan.ince@ogr.sakarya.edu.tr>
2019-01-20 23:59:03 +0300

    increment-version-function

--- awesome-config.sh
+++ awesome-config.sh
@@ -2,14 +2,14 @@
 # vim: noai:ts=4:sw=4:expandtab
 # shellcheck source=/dev/null
 # shellcheck disable=2009
-
+#
 # Awesome-Config
-# A command-line system config implementation tool
-# Written in bash 3.2+.
+# Copyright (c) 2019 Oguzhan Ince
+# A command-line system config implementation tool - Written in bash 3.2+.
+#
 # https://github.com/oguzhanlarca/awesome-config
 # http://www.wikizeroo.net/index.php?q=aHR0cHM6Ly9lbi53aWtpcGVkaWEub3JnL3dpa2kvSG9tZV9kaXJlY3Rvcnk
-#
-# Copyright (c) 2019 Oguzhan Ince
+
 
 # Default Configuration.
 bash_version="${BASH_VERSION/.*}"
@@ -22,12 +22,43 @@ LC_ALL=C
 LANG=C
 export GIO_EXTRA_MODULES="/usr/lib/x86_64-linux-gnu/gio/modules/"
 
-# Awesome-Config Major Version:
-version="1.0.0"
+SCRIPT_NAME="awesome-config.sh"
+ARGS="run"
+NEW_FILE="https://raw.githubusercontent.com/oguzhanlarca/awesome-config/master/awesome-config.sh"
+
+version="1.2.3.44" # Awesome-Config Version
+
+# check if there is a new version of this file
+# here, hypothetically we check if a file exists in the disk.
+# it could be an apt/yum check or whatever...
+check_upgrade () {
+[ -f "$NEW_FILE" ] && {
+
+# install a new version of this file or package
+# again, in this example, this is done by just copying the new file
+echo "Found a new version, updating ..."
+cp "$NEW_FILE" "$SCRIPT_NAME"
+# rm -f "$NEW_FILE"
+
+# note that at this point this file was overwritten in the disk
+# now run this very own file, in its new version!
+echo "Running the new version..."
+$SCRIPT_NAME $ARGS
+
+# now exit this old instance
+exit $?
+}
+
+echo 'Latest version...'
+}
+
+main () {
+echo 'Awesome-Config version: ' $version
+check_upgrade
 
 ### CASE ###
 case $1 in
-install)
+run)
 echo ' '
 echo 'OPTION: INSTALLING...'
 echo ' '
@@ -71,7 +102,7 @@ ___ __                          __   __
 
 ### DEFAULTS ###
 [ -z '$dotfilesrepo' ] && dotfilesrepo='https://github.com/oguzhanlarca/awesome-config.git'
-[ -z '$dotfilesfresh' ] && dotfilesfresh='https://github.com/oguzhanlarca/fresh-config.git'
+# [ -z '$dotfilesfresh' ] && dotfilesfresh='https://github.com/oguzhanlarca/fresh-config.git'
 
 # dir=$HOME
 # userdir=$HOME
@@ -144,22 +175,49 @@ echo '[ OK ]' || echo 'Awesome-Config Installing...'
 
 ### Installation
 putgitrepo $dotfilesrepo $HOME || error 'dotfilesrepo failed to deploy'
-putgitrepo $dotfilesfresh $HOME/.fresh-config || error  'dotfilesfresh failed to deploy'
+# putgitrepo $dotfilesfresh $HOME/.fresh-config || error  'dotfilesfresh failed to deploy'
 
 ### Complete
 finalize
 clear
 ;;
+
+### Update
 update)
 echo ' '
-echo 'OPTION: UPDATEING...'
-echo 'You are using latest Awesome-Config Version:' || echo $version
+
+check_upgrade
+echo -e '\033[1;37mYou are using latest Awesome-Config Version:' $version
+
+echo 'First state in this script: If there is new vers. then install it!.'
+echo 'This is a hard installation.' 'Do you want to continue? (yes/no): '
+read userread
+if [userread==yes]
+then
+echo -e '\033[1;33mOPTION:' '\033[0;37mUPDATEING...'
+else
+echo -e '\033[1;31mExiting the application.'
+fi
 ;;
+
+# increment_version case
+increment-version)
+echo ' '
+# increment_version function definition
+echo $version | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}'
+echo -e '\033[0;33mOld Value of Version:' '\033[0;37m'$version
+version=$(increment_version $version)
+echo -e '\033[1;33mNew Value of Version:' '\033[1;37m'$version
+echo -e '\033[1;31mExiting the application.'
+echo ' '
+;;
+
+# Any other entering:
 *)
 echo ' '
-echo 'OPTIONS: install or update'
-echo 'You are not using any option.'
+echo -e '\033[1;33mOPTIONS:' '\033[0;37m<run> <update> <increment-version> <man>'
+echo -e '\033[1;37mYou are not using any option.' '\033[1;31mExiting the application.'
 echo ' '
 ;;
 esac
-
+}
